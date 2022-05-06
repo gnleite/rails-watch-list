3003f045cffaf8ddb4f9f1c21e08a19fe44b8318
Gabriela Leite <nleite.gabriela@gmail.com>
2022-05-05 21:25:13 -0300

    changes

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ gem "font-awesome-sass", "~> 5.15"
 gem "simple_form", github: "heartcombo/simple_form"
 gem "rest-client"
 
-# gem "pg"
+gem "pg"
 
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.16.0'
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  has_one_attached :photo
 
   validates :movie_id, presence: true, uniqueness: { scope: :list_id }
   validates :comment, length: { minimum: 6 }
